60b788f37ad128fa3b226d9f14857e37ae65a6b8
Ben Sherratt <ben@vitei.com>
2014-10-02 17:00:45 +0900

    Add filters on init

--- src/filters/init.c
+++ src/filters/init.c
@@ -1,6 +1,14 @@
 #include "init.h"
+#include "../repo.h"
 
 int filtersInit(void)
 {
-	return 0;
+	git_config_set_string(gRepoConfigHandle,
+	                      "filter.media.clean", "git-big filter-clean");
+	git_config_set_string(gRepoConfigHandle,
+	                      "filter.media.smudge", "git-big filter-smudge");
+
+	// FIXME: need to find a way to make these filters required...
+
+	return kErrorNone;
 }
