d458cb8144ece6a269e65914669636d76239ec99
Ben Sherratt <ben@vitei.com>
2014-10-03 14:54:57 +0900

    Added pattern file checks

--- src/patterns.c
+++ src/patterns.c
@@ -1,7 +1,35 @@
+#include <stdio.h>
+#include <unistd.h>
+
 #include "patterns.h"
+#include "repo.h"
+
+static const char *kPatternsFile = ".gitbig";
+
+static const char *getPath(void);
 
 enum Error patternsInit(void)
 {
 	return kErrorNone;
+}
+
+bool patternsIsFilePresent(void)
+{
+	return access(getPath(), F_OK) != -1;
+}
+
+static const char *getPath(void)
+{
+	static char path[1024] = { '\0' };
+
+	if(!*path)
+	{
+		const char *workingDir = git_repository_workdir(gRepoHandle);
+
+		// FIXME: unsafe for large paths
+		snprintf(path, sizeof(path), "%s%s", workingDir, kPatternsFile);
+	}
+
+	return (const char *)path;
 }
 
--- src/patterns.h
+++ src/patterns.h
@@ -1,8 +1,11 @@
 #ifndef PATTERNS_H
 #define PATTERNS_H
 
+#include <stdbool.h>
 #include "errors.h"
 
 enum Error patternsInit(void);
+
+bool patternsIsFilePresent(void);
 
 #endif
