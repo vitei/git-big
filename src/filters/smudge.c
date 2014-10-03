f825f99785900869388f641d6563275cc50872fb
Ben Sherratt <ben@vitei.com>
2014-10-03 16:13:04 +0900

    Added smudge filter code

--- src/filters/smudge.c
+++ src/filters/smudge.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+
 #include "smudge.h"
+#include "../db.h"
 
 enum Error filterSmudgeRun(int argc, char *argv[])
 {
+	char hash[40];
+
+	fread(hash, 1, sizeof(hash), stdin);
+	dbQueryFile(hash, stdout);
+
 	return kErrorNone;
 }
 
