7477490126fa32d33dbb367956f9a7e5767b26eb
Ben Sherratt <ben@vitei.com>
2014-10-03 16:12:44 +0900

    Added clean filter code

--- src/filters/clean.c
+++ src/filters/clean.c
@@ -1,7 +1,15 @@
+#include <stdio.h>
+
 #include "clean.h"
+#include "../db.h"
 
 enum Error filterCleanRun(int argc, char *argv[])
 {
+	char hash[40];
+
+	dbInsertFile(stdin, hash);
+	fwrite(hash, 1, sizeof(hash), stdout);
+
 	return kErrorNone;
 }
 
