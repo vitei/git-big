35bb4909a7391c38ec9373cd86394730cb65d91b
Ben Sherratt <ben@vitei.com>
2014-10-03 13:04:29 +0900

    Copy-paste bug - filter is called big
    This should really be fixed earlier, but the large re-structure is a
    pain for that.

--- src/filters.c
+++ src/filters.c
@@ -4,9 +4,9 @@
 enum Error filtersInit(void)
 {
 	git_config_set_string(gRepoConfigHandle,
-	                      "filter.media.clean", "git-big filter-clean");
+	                      "filter.big.clean", "git-big filter-clean");
 	git_config_set_string(gRepoConfigHandle,
-	                      "filter.media.smudge", "git-big filter-smudge");
+	                      "filter.big.smudge", "git-big filter-smudge");
 
 	// FIXME: need to find a way to make these filters required...
 
