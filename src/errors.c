6dc9bc396c90c75cb982e8d78b5b978b6d646b6f
Ben Sherratt <ben@vitei.com>
2014-10-21 19:20:07 +0900

    Changed the order of some errors

--- src/errors.c
+++ src/errors.c
@@ -10,12 +10,11 @@ const char *error_string_table[ERROR_MAX] =
 	"Git repository is corrupt",
 
 	"Could not create the git-big database directory",
-
-	"Could not create .gitbig file",
-
 	"Could not find database file, please run git big sync"
 	"Could not insert file into database",
 	"Corrupt database file",
+
+	"Could not create .gitbig file",
 
 	"Filter input is invalid",
 };
