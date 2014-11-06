f8dbc7e0fb0d2d35c8dc8da84da245d79ceb8a70
Ben Sherratt <ben@vitei.com>
2014-11-06 16:53:05 +0900

    Missing comma caused bugs

--- src/errors.c
+++ src/errors.c
@@ -11,7 +11,7 @@ const char *error_string_table[ERROR_MAX] =
 	"Git repository is corrupt",
 
 	"Could not create the git-big database directory",
-	"Could not find database file, please run git big sync"
+	"Could not find database file, please run git big sync",
 	"Could not insert file into database",
 	"Corrupt database file",
 
