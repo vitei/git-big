8c40db1eca4cc3ebf146c7f1500d9c71d33e3b73
Ben Sherratt <ben@vitei.com>
2014-11-07 17:05:49 +0900

    Added big-pull to the smudge filter
    This is rather scrappy for now, but there seems to be quite a few issues
    with making the pull work post-checkout. I think it might be fixable and
    possibly better to do the big file pulls at that point, but for now this
    will serve.

--- src/filters/smudge.c
+++ src/filters/smudge.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "smudge.h"
 #include "../db.h"
+#include "../hooks.h"
 #include "../patterns.h"
+
+static const char * const HOOK_NAME = "big-pull";
 
 enum Error filter_smudge_run(int argc, char *argv[])
 {
@@ -27,8 +31,46 @@ enum Error filter_smudge_run(int argc, char *argv[])
 
 	r = db_file_query(stdout, id);
 
+	if(r == ERROR_DB_FILE_QUERY_COULD_NOT_FIND_FILE)
+	{
+		bool exists = false;
+		char hook_path[1024]; // XXX
+		char hash[DB_ID_HASH_SIZE + 1] = { '\0' }; // +1 for null
+		char db_path[1024];
+		char tmp[1024];
+		int error = 0;
+
+		exists = hook_full_path(hook_path, HOOK_NAME);
+
+		if(!exists)
+		{
+			goto skip_pull;
+		}
+
+		db_id_parse(NULL, hash, id);
+		hash[DB_ID_HASH_SIZE] = '\0';
+
+		db_file_path(db_path, sizeof(db_path), id);
+
+		// FIXME: unsafe for large paths
+		snprintf(tmp, sizeof(tmp), "%s %s %s", hook_path, hash, db_path);
+
+		error = system(tmp);
+
+		if(error != 0)
+		{
+			r = ERROR_INTERNAL;
+			goto error_system;
+		}
+
+		r = db_file_query(stdout, id);
+	}
+
+error_system:
 error_id_read:
 error_passthrough:
+
+skip_pull:
 
 	return r;
 }
