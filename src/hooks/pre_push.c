a9ccd6e8b0cc7bb762a6e02b7f96991bf03ed7c5
Ben Sherratt <ben@vitei.com>
2014-11-13 17:14:35 +0900

    Cleaned up the pre-push code
    Used fscanf to parse options.
    Made it have only one exit point.

--- src/hooks/pre_push.c
+++ src/hooks/pre_push.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <string.h>
 
 #include "big_push.h"
 #include "pre_push.h"
@@ -14,36 +13,18 @@ enum Error hooks_pre_push_run(int argc, char *argv[])
 	enum Error r = ERROR_NONE;
 	char tmp[1024] = { '\0' };
 	size_t tmp_size = 0;
-	const char *local_ref = NULL;
-	const char *local_sha1 = NULL;
-	const char *remote_ref = NULL;
-	const char *remote_sha1 = NULL;
+	char local_sha1[40 + 1] = { '\0' };
+	char remote_sha1[40 + 1] = { '\0' };
 	git_oid local_oid;
 	git_oid remote_oid;
 	enum Error bigfile_push_r = ERROR_NONE;
 
 	if(!hook_big_push_exists())
 	{
-		return ERROR_NONE;
+		goto skip_no_hook;
 	}
 
-	tmp_size = fread(tmp, 1, sizeof(tmp), stdin);
-
-	if(tmp_size == sizeof(tmp))
-	{
-		// Oh dear...
-		r = ERROR_INTERNAL;
-		goto error_fix_your_buffer_sizes;
-	}
-	else if(tmp_size == 0)
-	{
-		return ERROR_NONE;
-	}
-
-	local_ref = strtok(tmp, DELIM);
-	local_sha1 = strtok(NULL, DELIM);
-	remote_ref = strtok(NULL, DELIM);
-	remote_sha1 = strtok(NULL, DELIM);
+	fscanf(stdin, "%*s %40s %*s %40s", local_sha1, remote_sha1);
 
 	git_oid_fromstr(&local_oid, local_sha1);
 	git_oid_fromstr(&remote_oid, remote_sha1);
@@ -51,7 +32,7 @@ enum Error hooks_pre_push_run(int argc, char *argv[])
 	// FIXME: this shouldn't be needed...
 	if(git_oid_equal(&local_oid, &remote_oid))
 	{
-		return ERROR_NONE;
+		goto skip_git_oid_equal;
 	}
 
 	r = repo_tree_walk_bigfiles_for_push(&remote_oid, &local_oid, bigfile_perform_push, &bigfile_push_r);
@@ -63,8 +44,9 @@ enum Error hooks_pre_push_run(int argc, char *argv[])
 
 	r = bigfile_push_r;
 
+skip_git_oid_equal:
 error_repo_tree_walk_bigfiles_for_push:
-error_fix_your_buffer_sizes:
+skip_no_hook:
 
 	return r;
 }
